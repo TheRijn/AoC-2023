b1c99097a512cc5ae93ef4706dd2f7175d7a18ea
Marijn Doeve <marijn@doeve.me>
2023-12-04 22:40:55 +0100

    Refactor 3

--- day3.py
+++ day3.py
@@ -6,10 +6,6 @@ from collections import defaultdict
 class Day3:
     def __init__(self) -> None:
         self.grid: list[list[str]] = []
-        self.number_found = ""
-        self.adjacent_to_symbol = False
-
-        self.adjacent_sum = 0
 
         for input_line in fileinput.input():
             self.grid.append(list(input_line.strip()))
@@ -17,83 +13,59 @@ class Day3:
         self.x_max = len(self.grid[0])
         self.y_max = len(self.grid)
 
-        self.current_star = None
-
-        self.part_one()
-        self.part_two()
-
-    def part_two(self):
         self.number_found = ""
-        self.gears = defaultdict(list)
+
+        # Part 1
+        self.adjacent_to_symbol = False
+        self.adjacent_sum = 0
+
+        # Part 2
+        self.current_gear = None
+        self.gears: dict[tuple[int, int], list[int]] = defaultdict(list)
 
         for y, line in enumerate(self.grid):
             for x, chr in enumerate(line):
                 if chr.isdigit():
                     self.number_found += chr
-                    star = self.check_adjacent_to_star(x, y)
-                    if star:
-                        self.current_star = star
+                    gear = self.check_adjacency(x, y)
+                    if gear:
+                        self.current_gear = gear
                     continue
-                self.end_of_number_star()
-            self.end_of_number_star()
+                self.end_of_number()
+            self.end_of_number()
 
         gear_sum = 0
 
         for lst in self.gears.values():
-            if len(lst) != 2:
-                continue
-            gear_sum += lst[0] * lst[1]
-
-        print(f"Part 2: {gear_sum}")
-
-    def end_of_number_star(self):
-        if not self.number_found:
-            return
-        if self.current_star:
-            self.gears[self.current_star].append(int(self.number_found))
-        self.number_found = ""
-        self.current_star = None
-
-    def check_adjacent_to_star(self, x_n, y_n) -> tuple[int, int] | None:
-        for y in range(max(0, y_n - 1), min(self.y_max, y_n + 2)):
-            for x in range(max(0, x_n - 1), min(self.x_max, x_n + 2)):
-                if self.grid[y][x] != "*":
-                    continue
-                return (x, y)
-
-        return None
-
-    def part_one(self):
-        for y, line in enumerate(self.grid):
-            for x, chr in enumerate(line):
-                if chr.isdigit():
-                    self.number_found += chr
-                    self.check_adjacent_to_symbol(x, y)
-                    continue
-
-                self.end_of_number(x, y)
-
-            self.end_of_number(x, y)
+            if len(lst) == 2:
+                gear_sum += lst[0] * lst[1]
 
         print(f"Part 1: {self.adjacent_sum}")
+        print(f"Part 2: {gear_sum}")
 
-    def end_of_number(self, x: int, y: int):
+    def end_of_number(self) -> None:
         if not self.number_found:
             return
 
         if self.adjacent_to_symbol:
             self.adjacent_sum += int(self.number_found)
 
+        if self.current_gear:
+            self.gears[self.current_gear].append(int(self.number_found))
+
+        self.current_gear = None
         self.number_found = ""
         self.adjacent_to_symbol = False
 
-    def check_adjacent_to_symbol(self, x_n: int, y_n: int):
-        for y in range(max(0, y_n - 1), min(self.y_max, y_n + 2)):
-            for x in range(max(0, x_n - 1), min(self.x_max, x_n + 2)):
+    def check_adjacency(self, x_adj: int, y_adj: int) -> tuple[int, int] | None:
+        for y in range(max(0, y_adj - 1), min(self.y_max, y_adj + 2)):
+            for x in range(max(0, x_adj - 1), min(self.x_max, x_adj + 2)):
                 target = self.grid[y][x]
                 if not target.isdigit() and target != ".":
                     self.adjacent_to_symbol = True
-                    continue
+                    if target == "*":
+                        return (x, y)
+        return None
 
 
 if __name__ == "__main__":
